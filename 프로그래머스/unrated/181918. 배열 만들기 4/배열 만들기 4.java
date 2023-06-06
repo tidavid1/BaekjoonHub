0254fa3f5d10603a0018b0486184adc345c2a76e
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-06 19:03:46 +0900

    [unrated] Title: 배열 만들기 4, Time: 5.08 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        int i=0;
+        while (i < arr.length){
+            if(stk.isEmpty()){
+                stk.add(arr[i++]);
+            } else if(stk.get(stk.size()-1) < arr[i]){
+                stk.add(arr[i++]);
+            } else{
+                stk.remove(stk.size()-1);
+            }
+        }
+        return stk;
+    }
+}
