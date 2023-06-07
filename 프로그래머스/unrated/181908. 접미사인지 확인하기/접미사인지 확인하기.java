c3c1cf2d890770804a704e5f7da270639ef21bbb
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-07 11:24:44 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.07 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        ArrayList<String> suffix = new ArrayList<>();
+        for(int i=0;i<my_string.length();i++){
+            suffix.add(my_string.substring(i));
+        }
+        if(suffix.contains(is_suffix)){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
