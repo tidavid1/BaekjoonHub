584fdd8cfe75fe0ec487ca0f8543cadd1fb457d6
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-07 11:24:37 +0900

    [unrated] Title: 접미사 배열, Time: 0.22 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public ArrayList<String> solution(String my_string) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(int i=0;i<my_string.length();i++){
+            answer.add(my_string.substring(i));
+        }
+        Collections.sort(answer);
+        return answer;
+    }
+}
