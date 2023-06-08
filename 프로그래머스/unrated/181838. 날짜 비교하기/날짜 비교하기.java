2eb1d40b3189334434b8811046511de821bcb7ca
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-08 15:39:41 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.74 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,8 @@
+import java.time.*;
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        LocalDate t1 = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate t2 = LocalDate.of(date2[0], date2[1], date2[2]);
+        return t1.isBefore(t2)?1:0;
+    }
+}
