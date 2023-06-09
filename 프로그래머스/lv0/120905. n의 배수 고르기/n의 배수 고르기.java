feab85f3753f2f8f04f386d956507e7fb00207e1
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-09 12:01:37 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList<Integer> solution(int n, int[] numlist) {
+        ArrayList<Integer>  answer = new ArrayList<>();
+        for(int i:numlist){
+            if(i%n==0) answer.add(i);
+        }
+        return answer;
+    }
+}
