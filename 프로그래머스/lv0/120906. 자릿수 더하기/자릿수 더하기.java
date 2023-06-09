d8713271f6693137ad962c0ea2bd846b1783f935
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-09 12:01:30 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=0;i<Integer.toString(n).length();i++){
+            answer += Integer.toString(n).charAt(i) -'0';
+        }
+        return answer;
+    }
+}
