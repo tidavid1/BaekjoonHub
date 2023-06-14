485ae226c5cbc701aa8ef14c07e08662f1a10a18
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-14 17:59:39 +0900

    [level 1] Title: 예산, Time: 0.38 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        for(int i=0;i<d.length;i++){
+            budget -= d[i];
+            if(budget <0) return i;
+        }
+        return d.length;
+    }
+}
