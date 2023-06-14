b3986083a8619d1ae6fb59fc793cbfe9c7010e0b
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-14 16:02:34 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.10 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<=n;i++){
+            sb.append(i%2==1?"수":"박");
+        }
+        return sb.toString();
+    }
+}
