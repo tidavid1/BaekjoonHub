aeb271c4865caf7b68fa05ac8d67cd2e2982b60f
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-18 13:51:08 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPrime(int n){
+        for(int i=2;i<n;i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    public int solution(int[] nums) {
+        int answer = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                for(int k = j+1;k<nums.length;k++){
+                    answer += isPrime(nums[i]+nums[j]+nums[k]) ? 1:0;
+                }
+            }
+        }
+        return answer;
+    }
+}
