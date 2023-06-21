40a7fe1a0f1549512d9ab62139d6694d64dd807e
KyungMin Lee <85854384+tidavid1@users.noreply.github.com>
2023-06-21 12:06:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.23 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,10 +4,10 @@ class Solution {
         String answer ="";
         Map<String ,Integer> table = new HashMap<>();
         for(String s: completion){
-            table.put(s, table.get(s) == null ? 1 : table.get(s)+1);
+            table.put(s, table.getOrDefault(s, 0)+1);
         }
         for(String s: participant){
-            if(table.get(s) == null || table.get(s) == 0){
+            if(table.getOrDefault(s, 0) == 0){
                 answer = s;
                 break;
             }
