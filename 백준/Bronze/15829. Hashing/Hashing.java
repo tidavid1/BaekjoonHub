248bdcad38b2c09e17c5c0b21089587b345a054f
KyungMin Lee <hello.tidavid@gmail.com>
2024-01-07 16:58:15 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14220 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -6,16 +6,21 @@ import java.io.OutputStreamWriter;
 
 public class Main {
 
+    private static final int MOD_NUMBER = 1234567891;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
         String s = br.readLine();
         long result = 0;
+        long pow = 1;
         for (int i = 0; i < n; i++) {
-            result += (long) ((s.charAt(i) - '`') * Math.pow(31, i) % 1234567891);
+            result += (s.charAt(i) - '`') * pow % MOD_NUMBER;
+            pow = pow * 31 % MOD_NUMBER;
         }
-        bw.write(String.valueOf(result % 1234567891));
+        result %= MOD_NUMBER;
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
         br.close();
