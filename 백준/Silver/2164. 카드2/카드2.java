e2552ece74cbddc73fa93425e41a960925867ae5
KyungMin Lee <hello.tidavid@gmail.com>
2024-01-09 11:46:38 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45700 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+        while (queue.size() != 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        bw.write(String.valueOf(queue.peek()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
