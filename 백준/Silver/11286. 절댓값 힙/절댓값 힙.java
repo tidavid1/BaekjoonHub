7127b26d44f79319ed86a371a4acaf74844b749d
KyungMin Lee <hello.tidavid@gmail.com>
2024-02-04 17:18:02 +0900

    [Silver I] Title: 절댓값 힙, Time: 336 ms, Memory: 25780 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            int absO1 = Math.abs(o1);
+            int abs02 = Math.abs(o2);
+            if (absO1 == abs02) {
+                return Integer.compare(o1, o2);
+            } else {
+                return Integer.compare(absO1, abs02);
+            }
+        });
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            while (n-- > 0) {
+                int command = Integer.parseInt(br.readLine());
+                if (command == 0) {
+                    if (queue.isEmpty()) {
+                        sb.append(0);
+                    } else {
+                        sb.append(queue.poll());
+                    }
+                    sb.append("\n");
+                } else {
+                    queue.add(command);
+                }
+            }
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            bw.write(sb.toString());
+            bw.flush();
+        }
+    }
+
+}
