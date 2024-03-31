84f88d3c6bf7864ed45fe8efba6f2a554e40d291
KyungMin Lee <hello.tidavid@gmail.com>
2024-03-31 17:41:31 +0900

    [Silver III] Title: N과 M (1), Time: 284 ms, Memory: 22612 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int n;
+    private static int m;
+    private static int[] arr;
+    private static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            arr = new int[m];
+        } catch (IOException ignore) {
+        }
+        dfs(0);
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException ignore) {
+        }
+    }
+
+    public static void dfs(int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            arr[depth] = i;
+            if (verifyNumber(i, depth)) {
+                dfs(depth + 1);
+            }
+        }
+
+    }
+
+
+    private static boolean verifyNumber(int value, int depth) {
+        for (int i = 0; i < depth; i++) {
+            if (arr[i] == value) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
